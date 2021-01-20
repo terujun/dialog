2a71cee1589c3a225411280b7129543d94dfbfe2
terujun <terujun44@gmail.com>
2021-01-20 04:38:31 +0000

    fix Dockerfile

--- dialog.go
+++ dialog.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func postarticleHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Hello!")
+}
 
 func main() {
 	fmt.Println("Hello!")
+	http.HandleFunc("/postarticle", postarticleHandler)
+	http.ListenAndServe(":80", nil)
 }
