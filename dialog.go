f56485c1cb12f84d098a65ce98df944c885890c9
terujun <terujun44@gmail.com>
2021-01-24 02:05:29 +0000

    fix dialog.go

--- dialog.go
+++ dialog.go
@@ -33,148 +33,262 @@ type Appstart struct {
 	TriggerID           string      `json:"trigger_id"`
 }
 
-//modal作成用
-/*
-type Modalcontent struct {
-	TriggerID string `json:"trigger_id"`
-	View      struct {
-		Title struct {
-			Type string `json:"type"`
-			Text string `json:"text"`
-		} `json:"title"`
-		Submit struct {
-			Type string `json:"type"`
-			Text string `json:"text"`
-		} `json:"submit"`
-		Blocks []struct {
-			Type    string `json:"type"`
-			Element struct {
-				Type        string `json:"type"`
-				ActionID    string `json:"action_id"`
-				Placeholder struct {
-					Type string `json:"type"`
-					Text string `json:"text"`
-				} `json:"placeholder"`
-			} `json:"element,omitempty"`
-			Label struct {
-				Type string `json:"type"`
-				Text string `json:"text"`
-			} `json:"label,omitempty"`
-			Elements []struct {
-				Type  string `json:"type"`
-				Text  string `json:"text"`
-				Emoji bool   `json:"emoji"`
-			} `json:"elements,omitempty"`
-			Element struct {
-				Type    string `json:"type"`
-				Options []struct {
-					Text struct {
-						Type  string `json:"type"`
-						Text  string `json:"text"`
-						Emoji bool   `json:"emoji"`
-					} `json:"text"`
-					Value string `json:"value"`
-				} `json:"options"`
-				ActionID string `json:"action_id"`
-			} `json:"element,omitempty"`
-			Label struct {
-				Type  string `json:"type"`
-				Text  string `json:"text"`
-				Emoji bool   `json:"emoji"`
-			} `json:"label,omitempty"`
-			Element struct {
-				Type        string `json:"type"`
-				Placeholder struct {
-					Type  string `json:"type"`
-					Text  string `json:"text"`
-					Emoji bool   `json:"emoji"`
-				} `json:"placeholder"`
-				Options []struct {
-					Text struct {
-						Type  string `json:"type"`
-						Text  string `json:"text"`
-						Emoji bool   `json:"emoji"`
-					} `json:"text"`
-					Value string `json:"value"`
-				} `json:"options"`
-				ActionID string `json:"action_id"`
-			} `json:"element,omitempty"`
-			Label struct {
-				Type  string `json:"type"`
-				Text  string `json:"text"`
-				Emoji bool   `json:"emoji"`
-			} `json:"label,omitempty"`
-			Element struct {
-				Type        string `json:"type"`
-				Placeholder struct {
-					Type  string `json:"type"`
-					Text  string `json:"text"`
-					Emoji bool   `json:"emoji"`
-				} `json:"placeholder"`
-				Options []struct {
-					Text struct {
-						Type  string `json:"type"`
-						Text  string `json:"text"`
-						Emoji bool   `json:"emoji"`
-					} `json:"text"`
-					Value string `json:"value"`
-				} `json:"options"`
-				ActionID string `json:"action_id"`
-			} `json:"element,omitempty"`
-			Label struct {
-				Type  string `json:"type"`
-				Text  string `json:"text"`
-				Emoji bool   `json:"emoji"`
-			} `json:"label,omitempty"`
-			Element struct {
-				Type      string `json:"type"`
-				Multiline bool   `json:"multiline"`
-				ActionID  string `json:"action_id"`
-			} `json:"element,omitempty"`
-			Label struct {
-				Type  string `json:"type"`
-				Text  string `json:"text"`
-				Emoji bool   `json:"emoji"`
-			} `json:"label,omitempty"`
-		} `json:"blocks"`
-		Type string `json:"type"`
-	} `json:"view"`
-}
-*/
-
-/*
 func SendSlackModal(webhookurl string, TriggerID string) error {
-	//trigger iD取得
-	var modalcontent Modalcontent
-	modalcontent.TriggerID = appstart.TriggerID
-	//構造体からJSONを作成
-	slackBody, _ := json.Marshal(modalcontent)
+	//trigger iD登録
+	var modalcontent string = `
+	{
+		"trigger_id": TriggerID,
+		"view": {
+			"title": {
+				"type": "plain_text",
+				"text": "Regiter your meal"
+			},
+			"submit": {
+				"type": "plain_text",
+				"text": "Submit"
+			},
+			"blocks": [
+				{
+					"type": "input",
+					"element": {
+						"type": "plain_text_input",
+						"action_id": "image URL",
+						"placeholder": {
+							"type": "plain_text",
+							"text": "https://〜 Paste your uploaded image's URL"
+						}
+					},
+					"label": {
+						"type": "plain_text",
+						"text": "Image URL"
+					}
+				},
+				{
+					"type": "context",
+					"elements": [
+						{
+							"type": "plain_text",
+							"text": "事前にslackへ画像up→画像タップ→その他タップ→リンクをコピー",
+							"emoji": true
+						}
+					]
+				},
+				{
+					"type": "input",
+					"element": {
+						"type": "radio_buttons",
+						"options": [
+							{
+								"text": {
+									"type": "plain_text",
+									"text": "美味すぎ",
+									"emoji": true
+								},
+								"value": "umai"
+							},
+							{
+								"text": {
+									"type": "plain_text",
+									"text": "美味しくない",
+									"emoji": true
+								},
+								"value": "mazui"
+							}
+						],
+						"action_id": "radio_buttons-action"
+					},
+					"label": {
+						"type": "plain_text",
+						"text": "お勧めする？",
+						"emoji": true
+					}
+				},
+				{
+					"type": "input",
+					"element": {
+						"type": "static_select",
+						"placeholder": {
+							"type": "plain_text",
+							"text": "Select a genre",
+							"emoji": true
+						},
+						"options": [
+							{
+								"text": {
+									"type": "plain_text",
+									"text": "酒",
+									"emoji": true
+								},
+								"value": "sake"
+							},
+							{
+								"text": {
+									"type": "plain_text",
+									"text": "アジア",
+									"emoji": true
+								},
+								"value": "asia"
+							},
+							{
+								"text": {
+									"type": "plain_text",
+									"text": "フレンチ",
+									"emoji": true
+								},
+								"value": "french"
+							},
+							{
+								"text": {
+									"type": "plain_text",
+									"text": "イタリアン",
+									"emoji": true
+								},
+								"value": "italian"
+							},
+							{
+								"text": {
+									"type": "plain_text",
+									"text": "その他",
+									"emoji": true
+								},
+								"value": "other_genres"
+							}
+						],
+						"action_id": "static_select-action"
+					},
+					"label": {
+						"type": "plain_text",
+						"text": "ジャンル選択",
+						"emoji": true
+					}
+				},
+				{
+					"type": "input",
+					"element": {
+						"type": "static_select",
+						"placeholder": {
+							"type": "plain_text",
+							"text": "Select a website",
+							"emoji": true
+						},
+						"options": [
+							{
+								"text": {
+									"type": "plain_text",
+									"text": "Uber",
+									"emoji": true
+								},
+								"value": "uber"
+							},
+							{
+								"text": {
+									"type": "plain_text",
+									"text": "Doordash",
+									"emoji": true
+								},
+								"value": "doordash"
+							},
+							{
+								"text": {
+									"type": "plain_text",
+									"text": "Grubhub",
+									"emoji": true
+								},
+								"value": "grubhub"
+							},
+							{
+								"text": {
+									"type": "plain_text",
+									"text": "Yelp",
+									"emoji": true
+								},
+								"value": "yelp"
+							},
+							{
+								"text": {
+									"type": "plain_text",
+									"text": "Eater SF",
+									"emoji": true
+								},
+								"value": "eater"
+							},
+							{
+								"text": {
+									"type": "plain_text",
+									"text": "その他",
+									"emoji": true
+								},
+								"value": "other_websites"
+							}
+						],
+						"action_id": "static_select-action"
+					},
+					"label": {
+						"type": "plain_text",
+						"text": "注文サイト",
+						"emoji": true
+					}
+				},
+				{
+					"type": "input",
+					"element": {
+						"type": "plain_text_input",
+						"action_id": "store_name",
+						"placeholder": {
+							"type": "plain_text",
+							"text": "store name"
+						}
+					},
+					"label": {
+						"type": "plain_text",
+						"text": "店名"
+					}
+				},
+				{
+					"type": "input",
+					"element": {
+						"type": "plain_text_input",
+						"multiline": true,
+						"action_id": "plain_text_input-action"
+					},
+					"label": {
+						"type": "plain_text",
+						"text": "一言",
+						"emoji": true
+					}
+				}
+			],
+			"type": "modal"
+		}
+	}`
+	fmt.Println("modal request")
+	fmt.Println(modalcontent)
 	//Modalようリクエスト作成
-	req, err := http.NewRequest(http.MethodPost, webhookurl, bytes.NewBuffer(slackBody))
-	if err != nil {
-		return err
-	}
-	//httpヘッダ追加
-	req.Header.Add("Content-Type", "application/json")
+	/*	req, err := http.NewRequest(http.MethodPost, webhookurl, bytes.NewBuffer(modalcontent))
+		if err != nil {
+			return err
+		}
+		//httpヘッダ追加
+		req.Header.Add("Content-Type", "application/json")
 
-	client := &http.Client{Timeout: 10 * time.Second}
+		client := &http.Client{Timeout: 10 * time.Second}
 
-	//送信
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
+		//送信
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
 
-	//レスポンス確認
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(resp.Body)
-	if buf.String() != "ok" {
-		return errors.New("Non-ok response returned from Slack")
-	}
+		//レスポンス確認
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(resp.Body)
+		if buf.String() != "ok" {
+			return errors.New("Non-ok response returned from Slack")
+		}
+	*/
 	return nil
 
 }
-*/
 
 func postarticleHandler(w http.ResponseWriter, req *http.Request) {
 	//受信確認用
@@ -199,17 +313,11 @@ func postarticleHandler(w http.ResponseWriter, req *http.Request) {
 		log.Fatal(err)
 	}
 
-	//取得確認出力
-	fmt.Println("triggerid")
-	fmt.Println(appstart.TriggerID)
-
 	//Modalの送信
-	/*
-		err = SendSlackModal(modalopenURL, apps)
-		if err != nil {
-			log.Fatal(err)
-		}
-	*/
+	err = SendSlackModal(modalopenURL, appstart.TriggerID)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
 
