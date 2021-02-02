53751aef2c08d82a152750cde03c40e8117088c1
terujun <terujun44@gmail.com>
2021-02-02 07:17:00 +0000

    fix directry path

--- pkg/meal-slack-bot/slack/slack.go
+++ pkg/meal-slack-bot/slack/slack.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"path/filepath"
+	"strings"
 
 	"github.com/mattn/go-jsonpointer"
 	"github.com/terujun/dialog/pkg/meal-slack-bot/file"
@@ -102,10 +103,10 @@ func (repo *SlackRepository) OpenHydrationAddView(triggerID string) ([]byte, err
 
 func PostJSON(token string, command string, requestParamasJSON string) ([]byte, error) {
 	client := &http.Client{}
-	//req, _ := http.NewRequest("POST", "https://slack.com/api/"+command, strings.NewReader(requestParamasJSON))
-	//req.Header.Add("Content-type", "application/json")
-	//req.Header.Add("Authorization", "Bearer "+token)
-	req, _ := http.NewRequest("POST", "https://slack.com/api/views.open", nil)
+	req, _ := http.NewRequest("POST", "https://slack.com/api/"+command, strings.NewReader(requestParamasJSON))
+	req.Header.Add("Content-type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+token)
+	//req, _ := http.NewRequest("POST", "https://slack.com/api/views.open", nil)
 
 	resp, err := client.Do(req)
 	if err != nil {
