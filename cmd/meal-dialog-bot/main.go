1f50321d03674ddd43f0e7271bba1c74581345d4
Junichi Teruya <terujun44@gmail.com>
2021-02-24 03:55:50 +0000

    add print

--- cmd/meal-dialog-bot/main.go
+++ cmd/meal-dialog-bot/main.go
@@ -168,11 +168,10 @@ func gateway(c echo.Context, appConfig config.Config, configsDirPath string) err
 
 	//type別にコールバックIDを取得する
 	var iCallbackID interface{}
-	log.Printf("受信できてなくない？")
 	switch requestType {
 	case "shortcut":
 		iCallbackID, _ = jsonpointer.Get(payload, "/callback_id")
-	case "view_submit":
+	case "view_submission":
 		//中身確認用
 		fmt.Println("payloadJSON is")
 		fmt.Println(string([]byte(payloadJSON)))
